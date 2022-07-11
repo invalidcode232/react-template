2e95ba063d41af868f29f6afb71bc5d3729c9a00
invalidcode232 <invalidcode69@gmail.com>
2022-07-11 20:16:10 +0700

    init

---
+++ src/components/Box/Box.tsx
@@ -0,0 +1,9 @@
+type Props = {};
+
+import styles from './Box.module.css';
+
+const Box = (props: Props) => {
+    return <div>Box</div>;
+};
+
+export default Box;
---
+++ templates/component/TemplateName.tsx
@@ -0,0 +1,9 @@
+type Props = {};
+
+import styles from './TemplateName.module.css';
+
+const TemplateName = (props: Props) => {
+    return <div>TemplateName</div>;
+};
+
+export default TemplateName;
